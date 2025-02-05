961db0fc45e30f240b525f898516846d7a0f917b
yuyouyou032 <69706398+yuyouyou032@users.noreply.github.com>
2025-02-05 22:17:31 +0000

    added typical high-low range to plot.py
    it messes up the scale though...

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -34,11 +34,20 @@ def plot_water_levels(station, dates, levels):
 
 
 def plot_water_level_with_fit(station, dates, levels, p):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
     poly, d0 = polyfit(dates, levels, p)
     print(poly)
-    plt.plot(d0, levels, '.')
-    x1 = np.linspace(d0[0], d0[-1], 30)
+    plt.plot(num2date(d0), levels, '.')
+    x1 = np.linspace(d0[0], d0[-1], 90)
     plt.plot(num2date(x1), poly(x1))
     plt.title(station[0].name)
     plt.xticks(rotation=45);
+
+
+    # annotate typical range low-high
+    plt.plot(num2date(d0), [station[0].typical_range[0] for i in range(len(d0))])
+    plt.plot(num2date(d0), [station[0].typical_range[1] for i in range(len(d0))])
+
     plt.show()
