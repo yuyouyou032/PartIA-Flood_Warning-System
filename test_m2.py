2a29a20cc6b5cac178fdad5ff54c5c701c59e82e
yuyouyou032 <69706398+yuyouyou032@users.noreply.github.com>
2025-02-08 13:15:06 +0000

    updated test_m2

--- test_m2.py
+++ test_m2.py
@@ -3,6 +3,7 @@ import Task2C
 import Task2D
 import Task2E
 import Task2F
+import Task2G
 
 
 
@@ -20,4 +21,7 @@ def test_2E():
 
 def test_2F():
     Task2F.run()
+
+def test_2G():
+    Task2G.run()
 
