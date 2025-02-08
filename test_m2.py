9b04e089503ab2e662afca0ccb2983a053dd59be
yuyouyou032 <69706398+yuyouyou032@users.noreply.github.com>
2025-02-08 13:12:56 +0000

    Update test_m2.py

--- test_m2.py
+++ test_m2.py
@@ -1,7 +1,23 @@
 import Task2B
 import Task2C
+import Task2D
+import Task2E
+import Task2F
+
+
+
 def test_2B():
     Task2B.run() # no return
 
 def test_2C():
     Task2C.run()
+
+def test_2D():
+    Task2D.run()
+
+def test_2E():
+    Task2E.run()
+
+def test_2F():
+    Task2F.run()
+
